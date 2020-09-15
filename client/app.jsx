b898521c98f74de6c587a7af4bf117a37ba54a8f
Kevin Hu <hukxchen+git@gmail.com>
2020-09-15 13:14:06 -0500

    Render a data driven document (D3) map of Chicago using redlining geoJSON

--- client/app.jsx
+++ client/app.jsx
@@ -2,5 +2,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Svg from './Svg.jsx';
 
-ReactDOM.render(<>Hello there friend!</>, document.getElementById('app'));
+ReactDOM.render(
+  <>
+    Hello there friend!
+    <Svg />
+  </>,
+  document.getElementById('app'),
+);
