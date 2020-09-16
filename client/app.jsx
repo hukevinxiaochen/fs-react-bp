0d0ba7dca6f8b8651b1cfc177fac5c2afa7ea2fc
Kevin Hu <hukxchen+git@gmail.com>
2020-09-16 14:55:02 -0500

    :sparkles: feat: uses a redux store with react-redux, thunk, and logging middleware; trivial reducer in place

--- client/app.jsx
+++ client/app.jsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Svg from './Svg.jsx';
+import { Provider } from 'react-redux';
+import { store } from './state/store';
+import Svg from './Svg';
 
 ReactDOM.render(
-  <>
+  <Provider store={store}>
     Hello there friend!
     <Svg />
-  </>,
+  </Provider>,
   document.getElementById('app'),
 );
