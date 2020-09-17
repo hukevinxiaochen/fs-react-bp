d4e92aa3ec369000babb300be11d581f831f2a9c
Kevin Hu <hukxchen+git@gmail.com>
2020-09-17 17:43:03 -0500

    :sparkles: enables static file serving
    the unit test passes and so does the live browser test drive

--- server/app.js
+++ server/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
+
+// Logging middleware
 app.use(morgan('dev'));
+
+// Static file server
+app.use('/static', express.static(path.resolve(__dirname, '../public')));
+
+// // Serve index.html
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../public/index.html'));
+// });
+
+// Handle 404s
 app.use((req, res) => {
   const resourceNotFound =
     'The resource you requested could not be located on our servers';
   res.status(404).send(resourceNotFound);
 });
+
 module.exports = app;
