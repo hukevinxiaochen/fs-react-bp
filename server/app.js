892223a10faf61c005c6888a8bb543b9d30e61c6
Kevin Hu <hukxchen+git@gmail.com>
2020-09-17 17:14:41 -0500

    :sparkles: handle 404s correctly
    because we'd really like everyone to know if a resource requested could not be found.

--- server/app.js
+++ server/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
-
-app.use((req, res, next) => {
-  res.sendStatus(200);
+app.use(morgan('dev'));
+app.use((req, res) => {
+  const resourceNotFound =
+    'The resource you requested could not be located on our servers';
+  res.status(404).send(resourceNotFound);
 });
 module.exports = app;
