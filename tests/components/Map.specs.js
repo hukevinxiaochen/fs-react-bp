660d7d84cfef1b9ec50722c823d60bd8f7d2dbc3
Kevin Hu <hukxchen+git@gmail.com>
2020-09-17 16:20:19 -0500

    :white_check_mark: Add test for ToggleColorMode component
    because this needs to dispatch an action when clicked

--- tests/components/Map.specs.js
+++ tests/components/Map.specs.js
@@ -1,14 +1,16 @@
 import { expect } from 'chai';
-import { shallow, configure } from 'enzyme';
 import React from 'react';
 
+import { shallow, configure } from 'enzyme';
 // import Adapter
 import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
 
 // import component
 import Map from '../../client/Map';
 
-configure({ adapter: new Adapter() });
 
 describe('D3 map tests', () => {
   it('should render a redlining map of Chicago with <path> elements in our svg', function () {
--- tests/components/ToggleColorMode.specs.js
+++ tests/components/ToggleColorMode.specs.js
@@ -1,18 +1,30 @@
-// import { expect } from 'chai';
-import { shallow, configure } from 'enzyme';
+import { expect } from 'chai';
+import { mount, configure } from 'enzyme';
 import React from 'react';
-// 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { spy } from 'sinon';
+
 // // import Adapter
 // import Adapter from 'enzyme-adapter-react-16';
-// 
+//
 // // import component
 // import ToggleColorMode from '../../client/ToggleColorMode';
-
+// import store from '../../client/store';
+// import reducer from '../../client/reducer';
 // configure({ adapter: new Adapter() });
-
 
 describe('ToggleColorMode component', () => {
   it('dispatches a COLOR_TOGGLED action when clicked', () => {
-    const wrapper = shallow(<ToggleColorMode />);
+    const mockStore = createStore(reducer);
+    const dispatchSpiedUpon = spy(mockStore.dispatch)
+    const wrapper = mount(
+      <Provider store={mockStore}>
+        <ToggleColorMode />
+      </Provider>
+    );
+    wrapper.simulate('click');
+    const clickDidDispatchCOLOR_TOGGLEDAction = dispatchSpiedUpon.withArgs(COLOR_TOGGLED).calledOnce;
+    expect(clickDidDispatchCOLOR_TOGGLEDAction).to.be.true;
   });
 });
