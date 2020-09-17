a0fd1d48d07c05601e8062548be47e669c354684
Kevin Hu <hukxchen+git@gmail.com>
2020-09-17 17:42:19 -0500

    :white_check_mark: test to see that static file serving works
    because this should be in place before we try serving our main website. This does fail appropriately at first.

--- tests/api/server.specs.js
+++ tests/api/server.specs.js
@@ -2,9 +2,20 @@ import { expect } from 'chai';
 const request = require('supertest');
 const app = require('../../server/app');
 
-describe('GET /lasjdfkladj', () => {
-  it('handles non-existent resources with 404', (done) => {
-    // magic of supertest
-    request(app).get('/lasjdfkladj').expect(404, done);
+describe('Express Server', () => {
+  context('GET /lasjdfkladj', () => {
+    it('handles non-existent resources with 404', (done) => {
+      // magic of supertest
+      request(app).get('/lasjdfkladj').expect(404, done);
+    });
+  });
+
+  context('GET /static resources', () => {
+    it('serves static files', (done) => {
+      request(app)
+        .get('/static/my-first-svg.png')
+        .expect('Content-Type', 'image/png')
+        .expect(200, done);
+    });
   });
 });
