57e9cbd846ce8ec0add0baf5496491eb489a1b29
Kevin Hu <hukxchen+git@gmail.com>
2020-09-17 14:01:58 -0500

    :recycle: create setup.specs.js
    This file gets run first by mocha because it is top level. It has no tests, but does import all the stuff we need and helps us write cleaner component test files without having to repeat that boilerplate.

---
+++ tests/setup.specs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { expect } from 'chai';
+import sinon from 'sinon'; //spies and stubs
+
+// import Adapter
+import Adapter from 'enzyme-adapter-react-16';
+// import configure from Enzyme
+import { configure, shallow } from 'enzyme';
+// import the component to be tested
+import Map from '../../client/Map';
+
+/* Enzyme expects an adapter to be configured
+To configure an adapter, you should call `Enzyme.configure({  adapter: new Adapter() })`
+before using any of Enzyme's top level APIs, where `Adapter` is the adapter corresponding
+to the library currently being tested. For example:
+import Adapter from 'enzyme-adapter-react-16';
+To find out more about this, see http://airbnb.io/enzyme/docs/installation/index.html
+*/
+configure({ adapter: new Adapter() });
